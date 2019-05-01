4e05aec78fc4495ce37060b84823ee01c459c530
Agus <agustiyaninfo@gmail.com>
2019-05-01 09:29:53 +0700

    Rock Paper Scissors Game

---
+++ script.js
@@ -0,0 +1,84 @@
+function getPilihanComputer() {
+	const comp = Math.random();
+
+	if (comp < 0.34) return 'orang';
+	if (comp >= 0.34 && comp < 0.67) return 'gajah';
+	return 'semut';
+}
+
+function getHasil(comp, player) {
+	if (player == comp) return 'SERI';
+	if (player == 'gajah')
+		// if (comp == 'orang') {
+		// 	hasil = 'MENANG!';
+		// } else {
+		// 	hasil = 'KALAH!';
+		// }
+		return (comp == 'orang') ? 'MENANG!' : 'KALAH!';
+	if (player == 'orang') return (comp == 'gajah') ? 'KALAH!' : 'MENANG!';
+	if (player == 'semut') return (comp == 'orang') ? 'KALAH!' : 'MENANG!';
+	return 'memasukkan pilihan yang salah.';
+}
+
+const pilihan = document.querySelectorAll('li img');
+pilihan.forEach(function (pil) {
+	pil.addEventListener('click', function () {
+		const pilihanComputer = getPilihanComputer();
+		const pilihanPlayer = pil.className;
+		const hasil = getHasil(pilihanComputer, pilihanPlayer);
+
+		const imgComputer = document.querySelector('.img-komputer');
+		imgComputer.setAttribute('src', 'img/' + pilihanComputer + '.png');
+
+		const info = document.querySelector('.info');
+		info.innerHTML = hasil;
+	})
+})
+
+
+
+
+
+
+
+
+
+
+// const pGajah = document.querySelector('.gajah');
+// pGajah.addEventListener('click', function () {
+// 	const pilihanComputer = getPilihanComputer();
+// 	const pilihanPlayer = pGajah.className;
+// 	const hasil = getHasil(pilihanComputer, pilihanPlayer);
+
+// 	const imgComputer = document.querySelector('.img-komputer');
+// 	imgComputer.setAttribute('src', 'img/' + pilihanComputer + '.png');
+
+// 	const info = document.querySelector('.info');
+// 	info.innerHTML = hasil;
+// });
+
+// const pOrang = document.querySelector('.orang');
+// pOrang.addEventListener('click', function () {
+// 	const pilihanComputer = getPilihanComputer();
+// 	const pilihanPlayer = pOrang.className;
+// 	const hasil = getHasil(pilihanComputer, pilihanPlayer);
+
+// 	const imgComputer = document.querySelector('.img-komputer');
+// 	imgComputer.setAttribute('src', 'img/' + pilihanComputer + '.png');
+
+// 	const info = document.querySelector('.info');
+// 	info.innerHTML = hasil;
+// });
+
+// const pSemut = document.querySelector('.semut');
+// pSemut.addEventListener('click', function () {
+// 	const pilihanComputer = getPilihanComputer();
+// 	const pilihanPlayer = pSemut.className;
+// 	const hasil = getHasil(pilihanComputer, pilihanPlayer);
+
+// 	const imgComputer = document.querySelector('.img-komputer');
+// 	imgComputer.setAttribute('src', 'img/' + pilihanComputer + '.png');
+
+// 	const info = document.querySelector('.info');
+// 	info.innerHTML = hasil;
+// });
